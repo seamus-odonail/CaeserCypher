699591026de23b600af256fbcbc3db700319edb4
Alex Milender <alexmilender@gmail.com>
2021-11-07 16:03:57 -0600

    Adding Code Review comments for Assigment #2

--- src/od/james/EncryptionMachine.java
+++ src/od/james/EncryptionMachine.java
@@ -2,6 +2,8 @@ package od.james;
 
 import java.util.Locale;
 
+/* CODE REVIEW COMMENT: Please update this comment to be more descriptive on what the EncryptionMachine class is and its general functionality. */
+/* CODE REVIEW COMMENT: Please ensure that the comment has the same class name as the actual class. */
 /**
  * The encryption class
  */
@@ -10,10 +12,15 @@ public class EncryptionMachine {
     private final int encShift;
 
     public EncryptionMachine(int encryptionShift, String alphabet){
+        /* CODE REVIEW COMMENT: Nit 'enShift' could maybe updated to be more clear on what this variable is. */
         encShift = encryptionShift;
         charSet = alphabet;
     }
 
+    /* CODE REVIEW COMMENT: Nit could update @return to include the data type in which it is returning
+                            so that future users of this method know the return data type without looking in the method code. */
+    /* CODE REVIEW COMMENT: BUG This method allows and attempts to encrypt non-alphabet characters. This method should either raise
+                            an exception or indicate to the user that a non-alphabet characters can not be encrypted. */
     /**
      * Encrypt letter allows shifting in either a positive or negative direction
      * @param letter the original character to be encrypted
@@ -37,6 +44,8 @@ public class EncryptionMachine {
         return explodedAlphabet[newIdx];
     }
 
+    /* CODE REVIEW COMMENT: Nit could update @return to include the data type in which it is returning
+                        so that future users of this method know the return data type without looking in the method code. */
     /**
      * Allows for the encryption of a string
      * @param word a set of non-whitespace characters
--- src/od/james/Main.java
+++ src/od/james/Main.java
@@ -2,7 +2,7 @@ package od.james;
 
 import java.util.Locale;
 import java.util.Scanner;
-
+/* CODE REVIEW COMMENT: Please include a comment that describes the purpose for 'Main' and its general functionality. */
 public class Main {
 
     public static final int SHIFT = 3;
@@ -16,11 +16,15 @@ public class Main {
         if(!ENCRYPTION_KEY.equals(encryptionKey.toLowerCase(Locale.ROOT)))
         {
             System.out.println("Incorrect encryption key.");
+            /* CODE REVIEW COMMENT: It maybe better to throw an exception or return a specific value to indicate that the program did not complete execution. */
             return;
         }
+        /* CODE REVIEW COMMENT: The following 2 lines can be removed as they are not being used. */
         // System.out.println("Shift amount (positive or negative value): ");
         // int shiftAmount = userInput.nextInt();
         System.out.println("How many words is your message?: ");
+        /* CODE REVIEW COMMENT: If a user enters a values that is not an integer, the program throws a non-handled exception.
+                                Please add exception handling that will gracefully fail the program*/
         int wordCount = userInput.nextInt();
 
         EncryptionMachine encMachine = new EncryptionMachine(SHIFT, ALPHABET);
@@ -29,6 +33,7 @@ public class Main {
         for (int i = 1; i <= wordCount; i++) {
             System.out.println("Next word: ");
             String userWord = userInput.next();
+            /* CODE REVIEW COMMENT: Nit 'encWord' could maybe updated to be more clear on what this variable is. */
             String encWord = encMachine.encryptWord(userWord);
             encryptedMessage.append(encWord);
             if (i < wordCount) encryptedMessage.append(" ");
